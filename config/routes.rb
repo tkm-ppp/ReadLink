7f9bcb871dca80b2ae2fb68625d5bd144189f46e
root <root@pR>
2024-11-26 10:53:08 +0900

    httparty導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
 
+  get 'books', to: 'books#index' 
+
+
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
@@ -9,7 +12,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  
+
   resources :users, only: [:show]
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
