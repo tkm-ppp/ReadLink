92f9b9052a1500ebc7bfd0e057df958edb898925
root <root@pR>
2025-03-12 00:31:24 +0900

    ファビコンとPWA

---
+++ public/sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/script/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+  
+  self.addEventListener('activate', function(event) {
+    console.log('Service Worker activating.');
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      )
+    );
+  });
